0085a9fb339ee3c2fb4391a0e9ced439d514e3a8
Niklas Andréasson <andreasson.n@gmail.com>
2015-03-01 08:51:27 +0100

    Save req from invoice form

--- app/assets/javascripts/components/invoice_form.js.jsx
+++ app/assets/javascripts/components/invoice_form.js.jsx
@@ -1,14 +1,31 @@
 var InvoiceForm = React.createClass({
 
+  handleSubmit: function(e) {
+    e.preventDefault();
+
+    var invoice = {
+      customer: this.refs.customer.getDOMNode().value.trim(),
+      invoice_date: this.refs.invoice_date.getDOMNode().value.trim(),
+      last_pay_date: this.refs.last_pay_date.getDOMNode().value.trim(),
+    };
+
+    $.post('/invoices.json', { invoice: invoice }, function() {
+      console.log('Response', arguments);
+    });
+
+    console.log('Handle submit');
+  },
+
   sendFormToServer: function() {
     // ajax to server
   },
 
   render: function() {
     return (
-      <form className="invoice">
-        <input type="text" placeholder="Customer" />
-        <input type="date" />
+      <form className="invoice" onSubmit={ this.handleSubmit }>
+        <input type="text" placeholder="Customer" ref="customer" />
+        <input type="date" ref="invoice_date" />
+        <input type="date" ref="last_pay_date" />
         <button type="submit">Submit</button>
       </form>
     );
