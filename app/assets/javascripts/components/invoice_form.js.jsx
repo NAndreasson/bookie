da5dcefa14214c8db8d87d7919f00d099b687b9a
Niklas Andréasson <andreasson.n@gmail.com>
2015-03-02 21:04:43 +0100

    Experiment with react

--- app/assets/javascripts/components/invoice_form.js.jsx
+++ app/assets/javascripts/components/invoice_form.js.jsx
@@ -1,5 +1,17 @@
 var InvoiceForm = React.createClass({
 
+  getInitialState: function() {
+    return { rows: [] };
+  },
+
+  newRow: function() {
+    console.log('New Row');
+    var rows = this.state.rows;
+    rows.push({ desc: '', price: 0, units: 0 });
+
+    this.setState({ rows: rows });
+  },
+
   handleSubmit: function(e) {
     e.preventDefault();
 
@@ -21,11 +33,22 @@ var InvoiceForm = React.createClass({
   },
 
   render: function() {
+
+    var invoiceRows = this.state.rows.map(function(invoiceRow) {
+      return <NewInvoiceRow row={invoiceRow} />;
+    });
+
     return (
       <form className="invoice" onSubmit={ this.handleSubmit }>
         <input type="text" placeholder="Customer" ref="customer" />
         <input type="date" ref="invoice_date" />
         <input type="date" ref="last_pay_date" />
+
+        <input type="button" value="New row" onClick={ this.newRow } />
+        <ul>
+          {invoiceRows}
+        </ul>
+
         <button type="submit">Submit</button>
       </form>
     );
---
+++ app/assets/javascripts/components/new_invoice_row.js.jsx
@@ -0,0 +1,12 @@
+var NewInvoiceRow = React.createClass({
+
+  render: function() {
+    return (
+      <tr>
+        <td><input type="text" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+      </tr>
+    );
+  }
+});
