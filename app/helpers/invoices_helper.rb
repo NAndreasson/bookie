ddee44599729b454536ddfd2809df800d83f0b81
Niklas Andréasson <andreasson.n@gmail.com>
2015-03-20 21:04:01 +0100

    Work on view

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -3,4 +3,36 @@ module InvoicesHelper
   def total_price(price, units)
     number_to_currency(price * units)
   end
+
+  def sum_rows_total(rows)
+    total = 0
+
+    rows.each do |row|
+      total += row.units * row.price
+    end
+
+    number_to_currency(total)
+  end
+
+  def calc_tax(rows)
+    total = 0
+
+    rows.each do |row|
+      total += row.units * row.price
+    end
+
+
+    number_to_currency(total * 0.25)
+  end
+
+  def sum_total(rows)
+    total = 0
+
+    rows.each do |row|
+      total += row.units * row.price
+    end
+
+    number_to_currency(total * 1.25)
+  end
+
 end
