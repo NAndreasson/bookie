a47cfcd14f4e4f7acaf0d01998122cd274a3dc98
Niklas Andréasson <andreasson.n@gmail.com>
2015-04-03 21:41:28 +0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227154547) do
+ActiveRecord::Schema.define(version: 20150401190503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "street"
+    t.string   "postnr"
+    t.string   "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "invoice_rows", force: :cascade do |t|
     t.integer  "invoice_id"
