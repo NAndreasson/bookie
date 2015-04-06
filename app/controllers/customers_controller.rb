d7224e9946e51f1bba80682b865af47d1f1a89c4
Niklas Andréasson <andreasson.n@gmail.com>
2015-04-06 06:32:33 +0200

    Create show page for customer (from Landvetter)

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,7 @@
 class CustomersController < ApplicationController
 
+  before_action :set_customer, only: [:show]
+
   def index
     @customers = Customer.all
   end
@@ -14,7 +16,14 @@ class CustomersController < ApplicationController
     redirect_to action: "index"
   end
 
+  def show
+  end
+
   private
+
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
 
   def customer_params
     params.require(:customer).permit(:name, :street, :postnr, :city)
