93a79c1a05e762eccb4858d2bc0a4eb3ed333732
Niklas Andréasson <andreasson.n@gmail.com>
2015-04-07 05:09:40 -0400

    Add edit view for customer

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,13 +1,19 @@
 class CustomersController < ApplicationController
 
-  before_action :set_customer, only: [:show]
+  before_action :set_customer, only: [:show, :edit, :update, :destroy]
 
   def index
     @customers = Customer.all
   end
 
+  def show
+  end
+
   def new
     @customer = Customer.new
+  end
+
+  def edit
   end
 
   def create
@@ -16,7 +22,24 @@ class CustomersController < ApplicationController
     redirect_to action: "index"
   end
 
-  def show
+  def update
+    respond_to do |format|
+      if @customer.update(customer_params)
+        format.html { redirect_to @customer, notice: 'Customer was successfully updated.' }
+        format.json { render :show, status: :ok, location: @customer }
+      else
+        format.html { render :edit }
+        format.json { render json: @customer.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @customer.destroy
+    respond_to do |format|
+      format.html { redirect_to customers_url, notice: "Customer was successfully destroyed." }
+      format.json { hea :no_content }
+    end
   end
 
   private
